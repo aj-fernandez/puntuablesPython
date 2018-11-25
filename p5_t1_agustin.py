ed61127eda425f458dd39f6fb736be7ef37fc151
aj-fernandez <agustinjfd@outlook.es>
2018-11-25 14:22:51 +0100

    exercise 5 completed

---
+++ p5_t1_agustin.py
@@ -0,0 +1,63 @@
+# @author: ajfernandez
+# @last_edited: 25/11/18
+# @repo: https://github.com/aj-fernandez/
+
+argument = 0
+
+def loadData():
+
+    myDict = {}
+
+    target = input("Enter the name of the file: ")
+
+    with open(target) as source:
+        for line in source:
+            key, value = line.strip().split(',', maxsplit=1) # 1 delimiter per line
+            myDict[key] = value.strip() # With strip() we remove the spaces at
+                                        # beginning or end of the string
+
+    print(myDict)
+    print("\n")
+    menu()
+
+def saveData():
+
+    regNumber = int(input("Enter the number of pairs [key, value] that you want to save: "))
+
+    newDict = {}
+    for i in range (regNumber):
+        key = input("Enter the key: ").strip()
+        value = input("Enter her value: ").strip()
+        newDict[key] = value
+
+    print("\nThe generated dictionary is:\n ")
+    print(newDict)
+
+    newFile = input("Enter a new name file to save this dictionary: ")
+
+    records = newDict.items()
+
+    with open(newFile, "w") as target:
+        for key, value in records:
+            target.write(str(key + ", " + value + "\n"))
+
+    menu()
+
+def out():
+
+    print("\nBye bye!!\n")
+    exit(0)
+
+def menu():
+    global argument
+    print("\n1. To generate a dictionary from file (LOAD DATA).\n2. To generate \
+a file from dictionary (SAVE DATA).\n3. Exit.")
+    argument = int(input())
+    switcher = {
+        1: loadData,
+        2: saveData,
+        3: out,
+    }
+    func = switcher.get(argument, lambda: menu())
+    func()
+menu()
