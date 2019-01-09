36a14ef47f5bd82b42d93a8f444a9839c5c1f723
aj-fernandez <agustinjfd@outlook.es>
2019-01-09 12:18:30 +0100

    decod.

--- p1_2t_agustinv1.2.py
+++ p1_2t_agustinv1.2.py
@@ -4,7 +4,7 @@
 # @repo: https://github.com/aj-fernandez/
 
 coded_msg = ""
-
+dcoded_msg = ""
 msg = input("\nType a message: ").upper()
 slider = int(input("\nEnter the value of key-slider: "))
 
@@ -14,5 +14,11 @@ for char in msg:
     else:
         coded_msg += chr((ord(char) + slider - 65) % 26 + 65)
 
-print("\n\nEncrypted message:", coded_msg)
+for char in coded_msg:
+    if char == " ":
+        dcoded_msg +=  char
+    else:
+        dcoded_msg += chr((ord(char) - slider - 65) % 26 + 65)
 
+print("\n\nEncrypted message:", coded_msg)
+print("\nDecrypted message:", dcoded_msg)
